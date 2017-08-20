81437ba3314542375c95ad5a9aa91f2e9155da2c
Harri Feng <harrifeng@gmail.com>
2017-08-20 20:42:45 +0800

    finish pic

--- js/uml.js
+++ js/uml.js
@@ -1,6 +1,6 @@
 var svg = d3.select('#d3-class-diagram').append('svg')
     .attr({
-      width: 600,
+      width: 900,
       height: 600,
       id: 'diagram'
     });
@@ -10,10 +10,7 @@ d3.classDiagram.addMarkers(svg.append('defs'));
 var classes = [
   {
     x: 340, y: 420, width: 190,
-    classname: 'Client',
-    methods: [
-      'command()'
-    ]
+    classname: 'Client'
   },
 
   {
@@ -24,12 +21,17 @@ var classes = [
       'execute()'
     ]
   },
+  {
+    x: 700, y: 20, width: 190,
+    classname: 'Invoker'
+  },
+
 
   {
     x: 40, y: 220, width: 190,
-    classname: 'ConcreteClient',
+    classname: 'Receiver',
     methods: [
-      'command()'
+      'action()'
     ]
   },
 
@@ -37,7 +39,7 @@ var classes = [
     x: 340, y: 220, width: 190,
     classname: 'ConcreteCommand',
     attributes: [
-      'client'
+      'receiver'
     ],
     methods: [
       'execute()'
@@ -61,7 +63,7 @@ var connectors = [
 
   {
     points: [
-      {x: boxes.ConcreteClient.rightX(), y: boxes.ConcreteCommand.midY()},
+      {x: boxes.Receiver.rightX(), y: boxes.ConcreteCommand.midY()},
       {x: boxes.ConcreteCommand.x, y: boxes.ConcreteCommand.midY()}
     ],
     markerEnd: 'diamond'
@@ -69,12 +71,13 @@ var connectors = [
 
   {
     points: [
-      {x: boxes.ConcreteClient.midX(), y: boxes.ConcreteClient.y},
-      {x: boxes.ConcreteClient.midX(), y: boxes.Client.bottomY()}
+      {x: boxes.Command.rightX(), y: boxes.Invoker.midY()},
+      {x: boxes.Invoker.x, y: boxes.Invoker.midY()}
     ],
-    dashline: true,
-    markerEnd: 'triangle'
+    markerEnd: 'diamond'
   },
+
+
   {
     points: [
       {x: boxes.Command.midX(), y: boxes.ConcreteCommand.y},
